e9d30c80a290b9bb958df3d91a38fee3e2d84258
Kaillanny Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-09-26 07:19:39 -0300

    Update GeradorDeImigrantes.java

--- POO/papers.please/src/GeradorDeImigrantes.java
+++ POO/papers.please/src/GeradorDeImigrantes.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Random;
 
 public class GeradorDeImigrantes {
-    private static final Random RAND = new Random();
+    private static final Random aleatorio = new Random();
 
     private static final String[] NOMES = {"Victor", "Maria", "Sergei", "Katrina", "Zoran", "Elsa", "Javier"};
     private static final String[] PAISES = {"Arstotzka", "Kolechia", "Republia", "Obristan", "Antaria"};
@@ -16,34 +16,34 @@ public class GeradorDeImigrantes {
     // O método principal
     public static Imigrante gerarImigranteAleatorio(LocalDate dataAtual) {
         //Sorteando dados
-        String nomeSorteado = NOMES[RAND.nextInt(NOMES.length)];
-        String paisSorteado = PAISES[RAND.nextInt(PAISES.length)];
+        String nomeSorteado = NOMES[aleatorio.nextInt(NOMES.length)];
+        String paisSorteado = PAISES[aleatorio.nextInt(PAISES.length)];
 
         List<Documento> documentos = new ArrayList<>();
         boolean deveSerAceito = true; // Assume que o imigrante é bom por padrão
         boolean temErroNoDocumento = false; // Se tiver algum erro
 
-        if (RAND.nextDouble() < 0.30) {
+        if (aleatorio.nextDouble() < 0.30) {
             temErroNoDocumento = true;
             deveSerAceito = false; // Se há erro, a resposta correta do sistema é rejeitar
         }
 
         LocalDate dataDeValidade;
 
-        if (temErroNoDocumento && RAND.nextBoolean()) {
+        if (temErroNoDocumento && aleatorio.nextBoolean()) {
             // Causa um ERRO de VENCIMENTO no Passaporte
-            dataDeValidade = dataAtual.minusDays(1 + RAND.nextInt(365));
+            dataDeValidade = dataAtual.minusDays(1 + aleatorio.nextInt(365));
             temErroNoDocumento = false; // Se o erro já foi usado, guarda aqui
         } else {
             // Passaporte valido
-            dataDeValidade = dataAtual.plusDays(1 + RAND.nextInt(365));
+            dataDeValidade = dataAtual.plusDays(1 + aleatorio.nextInt(365));
         }
 
         // 80% de chance de ter identidade, 20% de chance de estar ausente
-        if (RAND.nextDouble() < 0.80) {
+        if (aleatorio.nextDouble() < 0.80) {
             LocalDate validadeIdentidade = dataAtual.plusYears(10);
             boolean erroDeConteudo = false;
-            if (deveSerAceito && RAND.nextDouble() < 0.15) {
+            if (deveSerAceito && aleatorio.nextDouble() < 0.15) {
                 erroDeConteudo = true;
                 deveSerAceito = false; // Se o nome estiver errado, o imigrante deve ser rejeitado
                 System.out.println("  ERRO! - Nome diferente na Identidade.");
@@ -64,4 +64,5 @@ public class GeradorDeImigrantes {
         return new Imigrante(nomeSorteado, paisSorteado, documentos, deveSerAceito);
 
     }
+
 }
